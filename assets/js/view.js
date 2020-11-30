a63d75a4d1b11972cd1dcdccbb5c5d889817d753
dipen1489 <dipen1489@gmail.com>
2020-11-30 22:54:55 +0530

    1st commit test
    1st commit test

---
+++ assets/js/view.js
@@ -0,0 +1,140 @@
+/*
+ * Prepare container view
+ * Version : v1.0.0
+*  @2020
+*  
+*  Author : Sagar S
+*  https://www.fiverr.com/sagarsudra
+*  
+*/
+
+
+	'use strict';
+	var viewFrames = [];
+	var viewSeats = [];
+	
+$( document ).ready(function() {
+	console.log( "ready!" );
+	start();
+	
+	const canvas 	= $('#canvas');
+	const controls= $('#controls');
+	
+	const selectedFrame = $('.frame.selected');
+	const selectedSeat 	= $('.seat.selected');
+	
+	
+	const frames = {title : "FRAME FINISH OPTIONS", id: 'frames', materials : [
+		{name : 'Wooden Frames', materials:[{title : 'Frame1',thumb:'assets/images/m1Frame.png', src:'./assets/materials/m1.png'},{title : 'Frame2', thumb:'assets/images/m2Feame.png', src:'./assets/materials/m2.png'}]}
+					]};
+	var allFrames = frames.materials;
+	
+	const seats = {title : "FRAME FINISH OPTIONS", id: 'seats', materials : [
+	{name : 'Leather Seats', materials:[{title : 'Seat1',thumb:'assets/images/m1Seat.png', src:'./assets/materials/m1.png'},{title : 'Seat2',thumb:'assets/images/m2Seat.png', src:'./assets/materials/m2.png'}]}
+			]};
+	
+	var allSeats = seats.materials;
+	
+	generateNestedHTML(controls.find('.frames'), allFrames,'frame');
+	
+	generateNestedHTML(controls.find('.seats'), allSeats,'seat');
+	
+	/*for(var i=0; i<allSeats.length; i++){
+		var span = $('<span>').attr({'title':allSeats[i].title});
+		var img = $('<img>').attr({'src':allSeats[i].src,class:'seat-img','data-id':i});
+		span.html(img);
+		controls.find('.seats').append(span);
+	}*/
+    	
+    	controls.on('click','.frames img', function(){
+    		var id = parseInt($(this).attr('data-id'));
+    		/*controls.find('.frames img.selected').removeClass('selected');
+    		$(this).addClass('selected');*/
+    		var img = $('<img>').attr({'src':$(this).attr('src'),class:''});
+    		selectedFrame.html(img);
+    		console.log(allFrames[id]);
+    		updateFrame(viewFrames[id]);
+    	});
+    	
+		controls.on('click','.seats img', function(){
+			var id = $(this).attr('data-id');
+    		/*controls.find('.seats img.selected').removeClass('selected');
+    		$(this).addClass('selected');*/
+    		var img = $('<img>').attr({'src':$(this).attr('src'),class:''});
+    		selectedSeat.html(img);
+    		updateSeat(viewSeats[id]);
+    	});
+		
+		$('a[data-toggle="tooltip"]').tooltip({
+		    animated: 'fade',
+		    placement: 'bottom',
+		    html: true
+		});
+		
+});
+
+function generateNestedHTML(container, objectArr, type){
+	
+	try{
+		objectArr = JSON.parse(objectArr);
+	}catch(e){}
+	
+	for(var j=0; j<objectArr.length; j++){
+		try{
+			var thisObj = objectArr[j];
+			if(thisObj.materials){
+				var headDiv = $('<div>').attr({'data-toggle':"tooltip", class:'border-bottom py-3 mb-2 sub-header'}).html(thisObj.name);
+				container.append(headDiv);
+				container.append(generateNestedHTML(container, thisObj.materials, type));
+			}else{
+				
+				var imgClass = 'frame-img test';
+				var dataId = 0;
+				if(type == 'frame'){
+					dataId = viewFrames.length;
+					viewFrames.push(objectArr[j]);
+				}else{
+					imgClass = 'seat-img';
+					dataId = viewSeats.length;
+					viewSeats.push(objectArr[j]);
+				}
+				
+				var span = $('<span>').attr({'title':objectArr[j].title, 'data-toggle':"tooltip"});
+				var img = $('<img>').attr({'src':objectArr[j].thumb,class:imgClass, 'data-id':dataId});
+				span.html(img);
+				container.append(span);
+			}
+		}catch(e){
+			console.error(e);
+		}
+	}
+	
+}
+
+function updateFrame(obj){
+	
+	console.log('Update Frame :',obj);
+	var mainMaterial = new THREE.TextureLoader().load(obj.src);
+	//seatObj.material.map = mainMaterial;
+	frameObj.material[1].map = mainMaterial;
+	//frameObj.material[2].map = mainMaterial;
+	//seatObj , frameObj;
+	
+	/* var canvas = document.getElementById('previewCanvas'),
+	context = canvas.getContext('2d');
+	
+	var base_image = new Image();
+	base_image.src = obj.src;
+	base_image.onload = function(){
+	    context.drawImage(base_image, 0, 0);
+	}   */
+}
+
+function updateSeat(obj){
+	console.log('Update Seat :',obj);
+	var mainMaterial = new THREE.TextureLoader().load(obj.src);
+	seatObj.material.map = mainMaterial;
+	//frameObj.material[0].map = mainMaterial;
+}
+
+		
---
+++ assets/js3d/script.js
@@ -0,0 +1,135 @@
+const clock = new THREE.Clock();
+var scene;
+const BACKGROUND_COLOR = 0xeeeeee;
+var camera;
+var renderer;
+var cameraControls ;
+var wheelScroll = 0;
+var mainObj = null;
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+var finalLatLngArray = [];
+var xTotal = 0;
+var zTotal = 0;
+var altReducerFactor = 2;
+var angleReducerFactor = 90;
+var buondingBox;
+var countI = 0;
+var lastClickedDrone = null;
+var lastClickedDroneMaterial = null;
+var MAP_WIDTH = 22300;
+var MAP_HEIGHT = 14800;
+
+function init()
+{
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(BACKGROUND_COLOR );
+  CameraControls.install( { THREE: THREE } );
+  
+  camera = new THREE.PerspectiveCamera( 56, window.innerWidth/window.innerHeight, 0.1, 10000 );
+  //camera.position.set( 0, 90, 0 );
+  camera.position.set( 0, 27, 76 );
+  
+  var myCanvas = document.getElementById("previewCanvas");
+  console.log(myCanvas.clientWidth);
+  console.log(myCanvas.clientHeight);
+  renderer = new THREE.WebGLRenderer({antialias: true, canvas: myCanvas});
+  console.log(window.devicePixelRatio);
+  renderer.setPixelRatio( 3 );
+  
+  camera.aspect = myCanvas.clientWidth / myCanvas.clientHeight;
+  camera.updateProjectionMatrix();
+  
+  //var widthCanvas = ConvertPercentageToPx(window.innerWidth , myCanvas.style.width.replace("%", ""));
+  //var heightCanvas = ConvertPercentageToPx(window.innerHeight , myCanvas.style.height.replace("%", ""));
+  renderer.setSize( myCanvas.clientWidth, myCanvas.clientHeight);
+  //renderer.setSize( window.innerWidth, window.innerHeight );
+  //document.body.appendChild( renderer.domElement ); 
+  
+  /* var myCanvas = document.getElementById("previewCanvas");
+  renderer = new THREE.WebGLRenderer({antialias: true, canvas: myCanvas});
+  renderer.setPixelRatio( window.devicePixelRatio );
+  console.log("canvas height : ",myCanvas);
+  var widthCanvas = ConvertPercentageToPx(window.innerWidth , myCanvas.style.width.replace("%", ""));
+  var heightCanvas = ConvertPercentageToPx(window.innerHeight , myCanvas.style.height.replace("%", ""));
+  renderer.setSize( widthCanvas, heightCanvas); */
+  
+  cameraControls = new CameraControls( camera, renderer.domElement );
+  cameraControls.dollyToCursor = true;
+  //cameraControls.maxPolarAngle = 1.24;
+  
+  var ambientLight = new THREE.AmbientLight( 0xcccccc, 4 );
+  scene.add( ambientLight );
+  
+  var directionalLightFront = new THREE.DirectionalLight( 0xcccccc, 0.3 );
+  directionalLightFront.position.set(0,60,90);
+  scene.add( directionalLightFront );
+  
+  var directionalLightBack = new THREE.DirectionalLight( 0xcccccc, 0.3 );
+  directionalLightBack.position.set(0,0,-90);
+  scene.add( directionalLightBack );
+}
+
+function ConvertPercentageToPx(mainData , percentage)
+{
+  return ((mainData * percentage) / 100)
+}
+
+function toRadians(degrees) {
+  return degrees * Math.PI / 180;
+};
+
+var seatObj , frameObj;
+
+function loadObject()
+{
+	var loader = new THREE.FBXLoader();
+	loader.load( './assets/models3d/chair.fbx', function ( object ) {
+	object.position.set(0,-25,0);
+		 object.traverse(function (child) {
+				if (child instanceof THREE.Mesh) {
+					metal = new THREE.TextureLoader().load('./assets/materials/metallic.png');
+					normal = new THREE.TextureLoader().load('./assets/materials/normal.png' );
+					rough = new THREE.TextureLoader().load( './assets/materials/roughness.png' );
+					mainMaterial = new THREE.TextureLoader().load( './assets/materials/m1.png' );
+					if(child.name == "polySurface29"){
+						seatObj = child;
+						child.material.metalnessMap = metal;
+						child.material.normalMap = normal;
+						child.material.roughnessMap = rough;
+						child.material.map = mainMaterial;
+						
+					}
+					else if(child.name == "polySurface35"){
+						frameObj = child;
+						child.material[1].metalnessMap = metal;
+						child.material[1].normalMap = normal;
+						child.material[1].roughnessMap = rough;
+						child.material[1].map = mainMaterial;
+					}
+					console.log(child);
+					child.material.needsUpdate = true;
+				}
+		}); 
+		scene.add( object );
+	} , undefined , function ( e ) {
+		console.log( e );
+	}); 
+	  
+}
+
+var animate = function animate() {
+  const delta = clock.getDelta();
+  const hasControlsUpdated = cameraControls.update( delta );
+    //console.log(camera.position);
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+};
+
+function start()
+{
+  init();
+  loadObject();
+  animate();
+}
+
